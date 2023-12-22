b97a83bac148fc46f64e18c40e88cd0dcaa988a1
Denton Kev <dentonkev122@gmail.com>
2023-12-23 00:49:03 +0800

    Linting

--- commands/general/channel.js
+++ commands/general/channel.js
@@ -1,18 +1,18 @@
 import { ChannelType, SlashCommandBuilder } from 'discord.js';
 
 const channelCommand = {
-  data: new SlashCommandBuilder()
-    .setName('channel')
-    .setDescription('channel command')
-    .addChannelOption((option) =>
-      option
-        .setName('channel')
-        .setDescription('Channel')
-        .setRequired(true)
-        .addChannelTypes(ChannelType.GuildVoice)
-    ),
+	data: new SlashCommandBuilder()
+		.setName('channel')
+		.setDescription('channel command')
+		.addChannelOption((option) =>
+			option
+				.setName('channel')
+				.setDescription('Channel')
+				.setRequired(true)
+				.addChannelTypes(ChannelType.GuildVoice),
+		),
 
-  async execute(interaction) {},
+	async execute(interaction) {},
 };
 
 export default channelCommand;
--- commands/music/queue.js
+++ commands/music/queue.js
@@ -2,23 +2,23 @@ import { useQueue } from 'discord-player';
 import { SlashCommandBuilder } from 'discord.js';
 
 const queueCommand = {
-  data: new SlashCommandBuilder()
-    .setName('queue')
-    .setDescription('View the current queue'),
+	data: new SlashCommandBuilder()
+		.setName('queue')
+		.setDescription('View the current queue'),
 
-  async execute(interaction) {
-    const queue = useQueue(interaction.guildId);
-    const channel = interaction.member.voice.channel;
+	async execute(interaction) {
+		const queue = useQueue(interaction.guildId);
+		const channel = interaction.member.voice.channel;
 
-    if (!channel) {
-      return await interaction.reply({
-        content: 'You must be in a voice channel to use this command',
-        ephemeral: true,
-      });
-    }
+		if (!channel) {
+			return await interaction.reply({
+				content: 'You must be in a voice channel to use this command',
+				ephemeral: true,
+			});
+		}
 
-    const queueArray = queue.tracks.toArray();
-  },
+		const queueArray = queue.tracks.toArray();
+	},
 };
 
 export default queueCommand;
