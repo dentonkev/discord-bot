a5472fb3ac4d779352d73513c1379278eeb19d3f
Denton Kev <dentonkev122@gmail.com>
2023-12-22 22:01:15 +1100

    Completed general implementation of resume command

---
+++ commands/music/resume.js
@@ -0,0 +1,45 @@
+import { useQueue } from 'discord-player';
+import { SlashCommandBuilder } from 'discord.js';
+
+const resumeCommand = {
+  data: new SlashCommandBuilder()
+    .setName('resume')
+    .setDescription('Resume the song that is currently paused'),
+
+  async execute(interaction) {
+    const queue = useQueue(interaction.guildId);
+    const channel = interaction.member.voice.channel;
+
+    if (!channel) {
+      return await interaction.reply({
+        content: 'You must be in a voice channel to use this command',
+        ephemeral: true,
+      });
+    }
+
+    if (!queue.node.isPaused()) {
+      return await interaction.replay({
+        content: 'The song is already paused',
+      });
+    }
+
+    if (queue.node.isPlaying()) {
+      return await interaction.replay({
+        content: 'Song is already playing',
+      });
+    }
+
+    try {
+      queue.node.resume();
+      await interaction.reply({
+        content: 'Song has been resumed successfully',
+      });
+    } catch (error) {
+      await interaction.reply({
+        content: 'An error has occured during execution',
+      });
+    }
+  },
+};
+
+export default resumeCommand;
