38ca6c19dba37de87c0c02564c5b9cff037836bb
Denton Kev <dentonkev122@gmail.com>
2023-12-23 00:48:31 +0800

    Add boiler plate code for new commands

---
+++ commands/music/previous.js
@@ -0,0 +1,45 @@
+import { useQueue } from 'discord-player';
+import { SlashCommandBuilder } from 'discord.js';
+
+const previousCommand = {
+  data: new SlashCommandBuilder()
+    .setName('previous')
+    .setDescription('Resume the song that is currently paused'),
+
+  async execute(interaction) {
+    const queue = useQueue(interaction.guildId);
+    const channel = interaction.member.voice.channel;
+
+    if (!channel) {
+      return await interaction.reply({
+        content: 'You must be in a voice channel to use this command',
+        ephemeral: true,
+      });
+    }
+
+    if (queue.node.isPlaying()) {
+      return await interaction.replay({
+        content: 'Song is already playing',
+      });
+    }
+
+    if (!queue.node.isPaused()) {
+      return await interaction.replay({
+        content: 'The song is not currently paused',
+      });
+    }
+
+    try {
+      queue.node.resume();
+      await interaction.reply({
+        content: 'Song has been resumed successfully',
+      });
+    } catch (error) {
+      await interaction.reply({
+        content: 'An error has occured during execution',
+      });
+    }
+  },
+};
+
+export default previousCommand;
