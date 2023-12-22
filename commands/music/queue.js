263d9c4fd5a5d9ad0868fc0de018c7f5424ae699
Denton Kev <dentonkev122@gmail.com>
2023-12-22 15:13:18 +1100

    Queue command basics

---
+++ commands/music/queue.js
@@ -0,0 +1,24 @@
+import { useQueue } from 'discord-player';
+import { BaseInteraction, SlashCommandBuilder } from 'discord.js';
+
+const queueCommand = {
+  data: new SlashCommandBuilder()
+    .setName('queue')
+    .setDescription('View the current queue'),
+
+  async execute(interaction) {
+    const queue = useQueue(interaction.guildId);
+    const channel = interaction.member.voice.channel;
+
+    if (!channel) {
+      return await interaction.reply({
+        content: 'You must be in a voice channel to use this command',
+        ephemeral: true,
+      });
+    }
+
+    const queueArray = queue.tracks.toArray();
+  },
+};
+
+export default queueCommand;
