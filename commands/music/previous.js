67f63494975e858a895eb0440de645bb724f822c
Denton Kev <dentonkev122@gmail.com>
2023-12-25 13:26:10 +0800

    Implementation of previous command

--- commands/music/previous.js
+++ commands/music/previous.js
@@ -1,46 +1,60 @@
-import { useQueue } from 'discord-player';
+import { useHistory, useMainPlayer } from 'discord-player';
 import { SlashCommandBuilder } from 'discord.js';
 
 const previousCommand = {
-	data: new SlashCommandBuilder()
-		.setName('previous')
-		.setDescription('Resume the song that is currently paused'),
+  data: new SlashCommandBuilder()
+    .setName('previous')
+    .setDescription('Enqueue the previous song'),
 
-	async execute(interaction) {
-		const queue = useQueue(interaction.guildId);
-		const channel = interaction.member.voice.channel;
+  async execute(interaction) {
+    const player = useMainPlayer();
+    const channel = interaction.member.voice.channel;
 
-		if (!channel) {
-			return await interaction.reply({
-				content: 'You must be in a voice channel to use this command',
-				ephemeral: true,
-			});
-		}
+    if (!channel) {
+      return await interaction.reply({
+        content: 'You must be in a voice channel to use this command',
+        ephemeral: true,
+      });
+    }
 
-		if (queue.node.isPlaying()) {
-			return await interaction.replay({
-				content: 'Song is already playing',
-			});
-		}
+    try {
+      const history = useHistory(interaction.guildId);
+      const prev = history.previousTrack;
 
-		if (!queue.node.isPaused()) {
-			return await interaction.replay({
-				content: 'The song is not currently paused',
-			});
-		}
+      if (!prev) {
+        return await interaction.reply({
+          content: 'There is no previous track',
+        });
+      }
 
-		try {
-			queue.node.resume();
-			await interaction.reply({
-				content: 'Song has been resumed successfully',
-			});
-		}
-		catch (error) {
-			await interaction.reply({
-				content: 'An error has occured during execution',
-			});
-		}
-	},
+      player.play(channel, prev, {
+        nodeOptions: {
+          metadata: {
+            channel: interaction.channel,
+            client: interaction.guild.members.me,
+            requestedBy: interaction.user,
+          },
+          bufferingTimeout: 15000,
+          leaveOnStop: true,
+          leaveOnStopCooldown: 5000,
+          leaveOnEnd: true,
+          leaveOnEndCooldown: 15000,
+          leaveOnEmpty: true,
+          leaveOnEmptyCooldown: 300000,
+          skipOnNoStream: true,
+        },
+      });
+
+      await interaction.reply({
+        content: 'Previous song has been succesfully enqueued',
+      });
+    } catch (error) {
+      await interaction.reply({
+        content: 'An error has occured during execution',
+      });
+      console.log(error);
+    }
+  },
 };
 
 export default previousCommand;
