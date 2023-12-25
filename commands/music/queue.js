a43f276e0aeeaca7796c1a3b67d58487811103fd
Denton Kev <dentonkev122@gmail.com>
2023-12-25 17:57:52 +0800

    Change the play command to the queue command

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,24 +1,72 @@
-import { useQueue } from 'discord-player';
+import { QueryType, useMainPlayer } from 'discord-player';
 import { SlashCommandBuilder } from 'discord.js';
 
 const queueCommand = {
-	data: new SlashCommandBuilder()
-		.setName('queue')
-		.setDescription('View the current queue'),
+  data: new SlashCommandBuilder()
+    .setName('queue')
+    .setDescription('Add a song to the queue')
+    .addStringOption((option) =>
+      option
+        .setName('song')
+        .setDescription('Name of song you want to enqueue')
+        .setRequired(true)
+    ),
 
-	async execute(interaction) {
-		const queue = useQueue(interaction.guildId);
-		const channel = interaction.member.voice.channel;
+  async execute(interaction) {
+    await interaction.deferReply();
 
-		if (!channel) {
-			return await interaction.reply({
-				content: 'You must be in a voice channel to use this command',
-				ephemeral: true,
-			});
-		}
+    const player = useMainPlayer();
+    const channel = interaction.member.voice.channel;
 
-		const queueArray = queue.tracks.toArray();
-	},
+    if (!channel) {
+      return await interaction.editReply({
+        content: 'You must be in a voice channel to use this command',
+      });
+    }
+
+    const song = interaction.options.getString('song');
+
+    try {
+      const songSearch = await player.search(song, {
+        requestedBy: interaction.user,
+        searchEngine: QueryType.AUTO,
+      });
+
+      if (!songSearch || !songSearch.hasTracks() || songSearch.isEmpty()) {
+        return await interaction.editReply({
+          content: `${song} could not be found`,
+        });
+      }
+
+      const res = await player.play(channel, songSearch, {
+        nodeOptions: {
+          metadata: {
+            channel: interaction.channel,
+            client: interaction.guild.members.me,
+            requestedBy: interaction.user,
+          },
+          bufferingTimeout: 15000,
+          leaveOnStop: true,
+          leaveOnStopCooldown: 5000,
+          leaveOnEnd: true,
+          leaveOnEndCooldown: 15000,
+          leaveOnEmpty: true,
+          leaveOnEmptyCooldown: 300000,
+          skipOnNoStream: true,
+        },
+      });
+
+      await interaction.editReply({
+        content: `Enqueueing **${res.track.title}** - ${res.track.author} (${res.track.duration})`,
+      });
+    } catch (error) {
+      await interaction.editReply({
+        content: 'An error has occured during execution',
+      });
+
+      console.log(error);
+    }
+  },
 };
 
 export default queueCommand;
