9e44f2041b0ae88d022fb05e444271596b5543f0
Denton Kev <dentonkev122@gmail.com>
2023-12-25 18:23:56 +0800

    Implemntation of restart command

--- commands/music/restart.js
+++ commands/music/restart.js
@@ -2,45 +2,52 @@ import { useQueue } from 'discord-player';
 import { SlashCommandBuilder } from 'discord.js';
 
 const restartCommand = {
-	data: new SlashCommandBuilder()
-		.setName('restart')
-		.setDescription('Resume the song that is currently paused'),
+  data: new SlashCommandBuilder()
+    .setName('restart')
+    .setDescription('Restart the current song'),
 
-	async execute(interaction) {
-		const queue = useQueue(interaction.guildId);
-		const channel = interaction.member.voice.channel;
+  async execute(interaction) {
+    const queue = useQueue(interaction.guildId);
+    const channel = interaction.member.voice.channel;
 
-		if (!channel) {
-			return await interaction.reply({
-				content: 'You must be in a voice channel to use this command',
-				ephemeral: true,
-			});
-		}
+    if (!channel) {
+      return await interaction.reply({
+        content: 'You must be in a voice channel to use this command',
+        ephemeral: true,
+      });
+    }
 
-		if (queue.node.isPlaying()) {
-			return await interaction.replay({
-				content: 'Song is already playing',
-			});
-		}
+    const clientChannel = interaction.guild.members.me.voice.channel;
 
-		if (!queue.node.isPaused()) {
-			return await interaction.replay({
-				content: 'The song is not currently paused',
-			});
-		}
+    if (!clientChannel) {
+      return await interaction.reply({
+        content: 'I must be in your voice channel to use this command',
+        ephemeral: true,
+      });
+    }
 
-		try {
-			queue.node.resume();
-			await interaction.reply({
-				content: 'Song has been resumed successfully',
-			});
-		}
-		catch (error) {
-			await interaction.reply({
-				content: 'An error has occured during execution',
-			});
-		}
-	},
+    if (!queue.node.isPlaying()) {
+      return await interaction.reply({
+        content: 'No song is currently playing',
+      });
+    }
+
+    try {
+      const track = queue.currentTrack;
+
+      queue.insertTrack(track, 0);
+      queue.node.skip();
+
+      queue.node.resume();
+      await interaction.reply({
+        content: `Restarting **${track.title}** - ${track.author} (${track.duration})`,
+      });
+    } catch (error) {
+      await interaction.reply({
+        content: 'An error has occured during execution',
+      });
+    }
+  },
 };
 
 export default restartCommand;
