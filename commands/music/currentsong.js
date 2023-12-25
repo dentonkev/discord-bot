e1f8b9913fc64647c950e92b141b7e0d4f541187
Denton Kev <dentonkev122@gmail.com>
2023-12-25 16:23:46 +0800

    Add artist and length to interaction reply

--- commands/music/currentsong.js
+++ commands/music/currentsong.js
@@ -2,45 +2,38 @@ import { useQueue } from 'discord-player';
 import { SlashCommandBuilder } from 'discord.js';
 
 const currentsongCommand = {
-	data: new SlashCommandBuilder()
-		.setName('currentsong')
-		.setDescription('Resume the song that is currently paused'),
+  data: new SlashCommandBuilder()
+    .setName('currentsong')
+    .setDescription('The song that is currently playing'),
 
-	async execute(interaction) {
-		const queue = useQueue(interaction.guildId);
-		const channel = interaction.member.voice.channel;
+  async execute(interaction) {
+    const queue = useQueue(interaction.guildId);
+    const channel = interaction.member.voice.channel;
 
-		if (!channel) {
-			return await interaction.reply({
-				content: 'You must be in a voice channel to use this command',
-				ephemeral: true,
-			});
-		}
+    if (!channel) {
+      return await interaction.reply({
+        content: 'You must be in a voice channel to use this command',
+        ephemeral: true,
+      });
+    }
 
-		if (queue.node.isPlaying()) {
-			return await interaction.replay({
-				content: 'Song is already playing',
-			});
-		}
+    if (!queue.node.isPlaying()) {
+      return await interaction.reply({
+        content: 'No song is currently playing',
+      });
+    }
 
-		if (!queue.node.isPaused()) {
-			return await interaction.replay({
-				content: 'The song is not currently paused',
-			});
-		}
-
-		try {
-			queue.node.resume();
-			await interaction.reply({
-				content: 'Song has been resumed successfully',
-			});
-		}
-		catch (error) {
-			await interaction.reply({
-				content: 'An error has occured during execution',
-			});
-		}
-	},
+    try {
+      const currTrack = queue.currentTrack;
+      await interaction.reply({
+        content: `**${currTrack.title}** - ${currTrack.author} (${currTrack.duration} is currently playing)`,
+      });
+    } catch (error) {
+      await interaction.reply({
+        content: 'An error has occured during execution',
+      });
+    }
+  },
 };
 
 export default currentsongCommand;
