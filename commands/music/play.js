85f1566d3e19cbb579dc0dadd03e9c502ecf44ef
Denton Kev <dentonkev122@gmail.com>
2024-01-02 23:06:32 +1100

    Implementation of player command

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { useMainPlayer } from 'discord-player';
+import { useMainPlayer, useQueue, QueryType } from 'discord-player';
 
 const playCommand = {
   data: new SlashCommandBuilder()
@@ -24,16 +24,60 @@ const playCommand = {
       });
     }
 
-    const clientChannel = interaction.guild.members.me.voice.channel;
+    const vc = player.client.channels.resolve(channel);
+    let queue = useQueue(interaction.guildId);
 
-    if (!clientChannel) {
-      return await interaction.editReply({
-        content: 'I must be in your voice channel to use this command',
-        ephemeral: true,
+    if (!queue) {
+      queue = player.nodes.create(vc.guild, {
+        nodeOptions: {
+          metadata: {
+            channel: interaction.channel,
+            client: interaction.guild.members.me,
+            requestedBy: interaction.user,
+          },
+          bufferingTimeout: 15000,
+          leaveOnStop: true,
+          leaveOnStopCooldown: 5000,
+          leaveOnEnd: true,
+          leaveOnEndCooldown: 15000,
+          leaveOnEmpty: true,
+          leaveOnEmptyCooldown: 300000,
+          skipOnNoStream: true,
+        },
       });
     }
 
+    if (!queue.channel) {
+      queue.connect(vc);
+    }
+
     const song = interaction.options.getString('song');
+
+    try {
+      const songSearch = await player.search(song, {
+        requestedBy: interaction.user,
+        searchEngine: QueryType.AUTO,
+      });
+
+      if (!songSearch || !songSearch.hasTracks() || songSearch.isEmpty()) {
+        return await interaction.editReply({
+          content: `${song} could not be found`,
+        });
+      }
+
+      queue.node.play(songSearch.tracks[0], { queue: false });
+
+      const track = songSearch.tracks[0];
+
+      await interaction.editReply({
+        content: `Playing **${track.title}** - ${track.author} (${track.duration})`,
+      });
+    } catch (error) {
+      await interaction.editReply({
+        content: 'An error has occured during execution',
+      });
+      console.log(error);
+    }
   },
 };
 
