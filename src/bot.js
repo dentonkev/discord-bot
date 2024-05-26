7c246adbd72a89afb5b433eda6e279db8fd5d500
Denton Kev <dentonkev122@gmail.com>
2024-05-26 13:38:28 +1000

    updated packages

--- src/bot.js
+++ src/bot.js
@@ -14,7 +14,7 @@ import { fileURLToPath } from 'url';
 config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID;
+// const GUILD_ID = process.env.GUILD_ID;
 
 // Client initialization
 const client = new Client({
